39045acab5ce6969a38e01df5c82e1405280267e
insegnantePC <insegnante@fakemail.com>
2025-03-07 10:56:01 +0100

    Absolute paths and other minor changes

--- js/components.js
+++ js/components.js
@@ -1,10 +1,10 @@
 function loadHeader() {
-    fetch('header.html').then(response => response.text())
+    fetch('/header.html').then(response => response.text())
     .then(html => document.getElementById('header-container').innerHTML = html);
 }
 
 function loadFooter() {
-    fetch('footer.html').then(response => response.text())
+    fetch('/footer.html').then(response => response.text())
     .then(html => document.getElementById('footer-container').innerHTML = html);
 }
 
